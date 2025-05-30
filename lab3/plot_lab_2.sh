2e7be7714cf6b6c8347ff92d6304e3e11fdc54ed
marcinmik10 <marcin2003@outlook.com>
2025-05-30 15:52:16 +0200

    poprawki

--- lab3/plot_lab_2.sh
+++ lab3/plot_lab_2.sh
@@ -1,4 +1,4 @@
-set term pngcairo size 4000, 2700 enhanced font "Helvetica,30"
+set term pngcairo size 2000, 2700 enhanced font "Helvetica, 30"
 set output "trajektorie_wszystko.png"
 set grid lw 1.5
 set size square
@@ -8,8 +8,8 @@ set multiplot layout 3,2
 
 # y(x)
 set title "y(x) - trajektorie w płaszczyźnie xy"
-set xlabel "x"
-set ylabel "y"
+set xlabel "x[m]"
+set ylabel "y[m]"
 plot "traj0.dat" u 7:8 w l lw my_lw t 'traj0', \
      "traj1.dat" u 7:8 w l lw my_lw t 'traj1', \
      "traj2.dat" u 7:8 w l lw my_lw t 'traj2', \
@@ -17,8 +17,8 @@ plot "traj0.dat" u 7:8 w l lw my_lw t 'traj0', \
 
 # E(t)
 set title "E(t) - energia całkowita"
-set xlabel "t"
-set ylabel "E"
+set xlabel "t[s]"
+set ylabel "E[J]"
 plot "traj0.dat" u 1:11 w l lw my_lw t 'traj0', \
      "traj1.dat" u 1:11 w l lw my_lw t 'traj1', \
      "traj2.dat" u 1:11 w l lw my_lw t 'traj2', \
@@ -26,8 +26,8 @@ plot "traj0.dat" u 1:11 w l lw my_lw t 'traj0', \
 
 # r(t)
 set title "r(t)"
-set xlabel "t"
-set ylabel "r"
+set xlabel "t[s]"
+set ylabel "r[m]"
 plot "traj0.dat" u 1:2 w l lw my_lw t 'traj0', \
      "traj1.dat" u 1:2 w l lw my_lw t 'traj1', \
      "traj2.dat" u 1:2 w l lw my_lw t 'traj2', \
@@ -35,8 +35,8 @@ plot "traj0.dat" u 1:2 w l lw my_lw t 'traj0', \
 
 # fi(t)
 set title "phi(t)"
-set xlabel "t"
-set ylabel "phi"
+set xlabel "t[s]"
+set ylabel "phi[rad]"
 plot "traj0.dat" u 1:3 w l lw my_lw t 'traj0', \
      "traj1.dat" u 1:3 w l lw my_lw t 'traj1', \
      "traj2.dat" u 1:3 w l lw my_lw t 'traj2', \
@@ -44,8 +44,8 @@ plot "traj0.dat" u 1:3 w l lw my_lw t 'traj0', \
 
 # pr(t)
 set title "p_r(t)"
-set xlabel "t"
-set ylabel "p_r"
+set xlabel "t[s]"
+set ylabel "p_r[kg*m/s]"
 plot "traj0.dat" u 1:5 w l lw my_lw t 'traj0', \
      "traj1.dat" u 1:5 w l lw my_lw t 'traj1', \
      "traj2.dat" u 1:5 w l lw my_lw t 'traj2', \
@@ -53,8 +53,8 @@ plot "traj0.dat" u 1:5 w l lw my_lw t 'traj0', \
 
 # pphi(t)
 set title "p_phi(t)"
-set xlabel "t"
-set ylabel "p_phi"
+set xlabel "t[s]"
+set ylabel "p_phi[kg*m^2/s]"
 plot "traj0.dat" u 1:6 w l lw my_lw t 'traj0', \
      "traj1.dat" u 1:6 w l lw my_lw t 'traj1', \
      "traj2.dat" u 1:6 w l lw my_lw t 'traj2', \
@@ -72,8 +72,8 @@ set multiplot layout 3,2
 
 # y(x)
 set title "y(x) - trajektorie w płaszczyźnie xy"
-set xlabel "x"
-set ylabel "y"
+set xlabel "x[m]"
+set ylabel "y[m]"
 plot "traj4.dat" u 7:8 w l lw my_lw t 'traj4', \
      "traj5.dat" u 7:8 w l lw my_lw t 'traj5', \
      "traj6.dat" u 7:8 w l lw my_lw t 'traj6', \
@@ -81,8 +81,8 @@ plot "traj4.dat" u 7:8 w l lw my_lw t 'traj4', \
 
 # E(t)
 set title "E(t) - energia całkowita"
-set xlabel "t"
-set ylabel "E"
+set xlabel "t[s]"
+set ylabel "E[J]"
 plot "traj4.dat" u 1:11 w l lw my_lw t 'traj4', \
      "traj5.dat" u 1:11 w l lw my_lw t 'traj5', \
      "traj6.dat" u 1:11 w l lw my_lw t 'traj6', \
@@ -90,8 +90,8 @@ plot "traj4.dat" u 1:11 w l lw my_lw t 'traj4', \
 
 # r(t)
 set title "r(t)"
-set xlabel "t"
-set ylabel "r"
+set xlabel "t[s]"
+set ylabel "r[m]"
 plot "traj4.dat" u 1:2 w l lw my_lw t 'traj4', \
      "traj5.dat" u 1:2 w l lw my_lw t 'traj5', \
      "traj6.dat" u 1:2 w l lw my_lw t 'traj6', \
@@ -99,8 +99,8 @@ plot "traj4.dat" u 1:2 w l lw my_lw t 'traj4', \
 
 # fi(t)
 set title "phi(t)"
-set xlabel "t"
-set ylabel "phi"
+set xlabel "t[s]"
+set ylabel "phi[rad]"
 plot "traj4.dat" u 1:3 w l lw my_lw t 'traj4', \
      "traj5.dat" u 1:3 w l lw my_lw t 'traj5', \
      "traj6.dat" u 1:3 w l lw my_lw t 'traj6', \
@@ -108,8 +108,8 @@ plot "traj4.dat" u 1:3 w l lw my_lw t 'traj4', \
 
 # pr(t)
 set title "p_r(t)"
-set xlabel "t"
-set ylabel "p_r"
+set xlabel "t[s]"
+set ylabel "p_r[kg*m/s]"
 plot "traj4.dat" u 1:5 w l lw my_lw t 'traj4', \
      "traj5.dat" u 1:5 w l lw my_lw t 'traj5', \
      "traj6.dat" u 1:5 w l lw my_lw t 'traj6', \
@@ -117,8 +117,8 @@ plot "traj4.dat" u 1:5 w l lw my_lw t 'traj4', \
 
 # pphi(t)
 set title "p_phi(t)"
-set xlabel "t"
-set ylabel "p_phi"
+set xlabel "t[s]"
+set ylabel "p_phi[kg*m^2/s]"
 plot "traj4.dat" u 1:6 w l lw my_lw t 'traj4', \
      "traj5.dat" u 1:6 w l lw my_lw t 'traj5', \
      "traj6.dat" u 1:6 w l lw my_lw t 'traj6', \
